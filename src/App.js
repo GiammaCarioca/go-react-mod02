ed7b799911613d442abe3aa95f6e611d39cca603
Thiago Giammattey <giammattey@gmail.com>
2018-08-23 11:08:59 -0300

    instalando styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import './styles/global';
+
+const Title = styled.h1`
+  color: #fff;
+  font-size: 32px;
+`;
 
 const App = () => (
   <div className="App">
-    <h1>Hello World</h1>
+    <Title>Hello World</Title>
   </div>
 );
 
